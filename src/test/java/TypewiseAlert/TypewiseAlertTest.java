23386128f286d3ce7b56b2a432c5ce40cdbb0e7a
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-04-01 11:04:54 +0530

    Update TypewiseAlertTest.java

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -6,10 +6,21 @@ import org.junit.Test;
 
 public class TypewiseAlertTest 
 {
-    @Test
+   @Test
     public void infersBreachAsPerLimits()
     {
-      assertTrue(TypewiseAlert.inferBreach(12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
+      assertTrue(TypewiseAlert.inferBreach(12, 20, 30) ==BreachType.TOO_LOW);
+    }
+	
+	@Test
+    public void checkAndAlertAsPerCoolingType()
+    {
+        BatteryCharacter batteryCharacter = new BatteryCharacter(CoolingType.PASSIVE_COOLING,"Bosch");
+		TypewiseAlert.checkAndAlert(AlertTarget.TO_Email,batteryCharacter , 36);
+    }
+	@Test
+    public void classifyTemperatureBreachAsPerCooling()
+    {
+        assertTrue((TypewiseAlert.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING,45) == BreachType.TOO_HIGH));
     }
 }
