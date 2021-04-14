f6781240eeb2892403253e8fd384b094a7456a6f
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-04-14 21:28:47 +0530

    Update EmailAlert.java

--- src/main/java/TypewiseAlert/EmailAlert.java
+++ src/main/java/TypewiseAlert/EmailAlert.java
@@ -2,10 +2,13 @@ package TypewiseAlert;
 
 public class EmailAlert implements IAlerter{
 	@Override
-	public void sendAlert(BreachType breachType) {
+	public String sendAlert(BreachType breachType) {
 		String recepient = "a.b@c.com";
 	    	  System.out.printf("To: %s%n", recepient);
+	    	  if(!breachType.getDisplayName().equalsIgnoreCase("normal")) {
 	          System.out.println("Hi, the temperature is "+breachType.getDisplayName()+"\n");
+	    	  }
+	    	return breachType.getDisplayName();
 	}
 
 }
