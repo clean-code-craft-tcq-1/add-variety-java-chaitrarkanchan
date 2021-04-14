6bca244616eaa0dd6a904755547713e8d52c8c08
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-04-14 21:28:18 +0530

    Update ControllerAlert.java

--- src/main/java/TypewiseAlert/ControllerAlert.java
+++ src/main/java/TypewiseAlert/ControllerAlert.java
@@ -3,10 +3,12 @@ package TypewiseAlert;
 public class ControllerAlert implements IAlerter {
 
 	@Override
-	public void sendAlert(BreachType breachType) {
+	public String sendAlert(BreachType breachType) {
 		// TODO Auto-generated method stub
 		  int header = 0xfeed;
 		  System.out.printf("%i : %i%n", header, breachType);
+		  return breachType.getDisplayName();
 	}
+
 
 }
